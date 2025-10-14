166899864805ff12c1c6c042a01a6addff350e32
rifathosenofficial2023 <rifathosenofficial@gmail.com>
2025-10-14 21:40:03 +0600

    leap year problem solve

--- problem_13.py
+++ problem_13.py
@@ -1,1 +1,7 @@
-print('hellow ')
+#How to check leap year 
+
+year = int(input("Enter your old "))
+if(year%4==0 and year%100!=0) or (year%400==0):
+    print(year,'It is leap year')
+else:
+    print('It is not leap year')
